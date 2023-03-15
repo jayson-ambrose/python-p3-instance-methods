a2f6b6e07cd04dac2347be9654bc39389d605036
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-15 15:20:02 -0600

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
 class Dog:
+
     # Class body goes here
+    
 
     #Instance method definition
+    def bark(self):
+        print('Woof!')
+
+    def no_barking(self):
+        print("*Barks anyway")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    pass
+
+class Person:
+
+    def talk (self):
+        print("Hello World!")
+
+    def walk (self):
+        print("The person is walking.")
+
     pass
